13f90cfa26ba62168f7e1a784e94eeb7a8fe1a7f
Khazin <Khazin316@gmail.com>
2018-10-22 11:02:01 +0800

    add style

--- bulkemail.php
+++ bulkemail.php
@@ -4,6 +4,7 @@ require 'vendor/autoload.php';
 if (isset($_POST['submit'])) {
   $emails = explode(",", $_POST['email']);
   foreach ($emails as $email) {
+    //include smtp.php file
     include_once 'vendor/phpmailer/phpmailer/examples/smtp.php';
   }
   if (!$mail->send()) {
@@ -19,11 +20,25 @@ if (isset($_POST['submit'])) {
     <meta charset="utf-8">
     <title></title>
   </head>
-  <body>
-<form class="" action="" method="post">
-  <label for="email">Email</label>
-  <textarea name="email" multiple cols="30" rows="5"></textarea>
+  <body style="background-color:#00ffff">
+    <div class="container">
+<form class="myform" action="" method="post">
+  <label for="email">Send to:</label><br>
+  <textarea name="email" multiple cols="30" rows="5"></textarea><br>
   <input type="submit" name="submit" value="Send">
 </form>
   </body>
+  <footer>
+    <h6>By: Khazin</h6>
+  </footer>
+</div>
 </html>
+
+<style media="screen">
+.container {
+    position: relative;
+    text-align: center;
+    border-radius: 5px;
+    top: 100px;
+}
+</style>
